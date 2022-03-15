68ad78acaec4ea2440b1a081737fa5e352e7a5b1
tickernelz <zhafronadani@gmail.com>
2022-03-15 22:56:38 +0700

    create pelaminan model
    Signed-off-by: tickernelz <zhafronadani@gmail.com>

---
+++ models/pelaminan.py
@@ -0,0 +1,15 @@
+from odoo import fields, models, api
+
+
+class Pelaminan(models.Model):
+    _name = 'toko.pelaminan'
+    _description = 'Pelaminan'
+
+    name = fields.Char(
+        string='Name')
+    deskripsi = fields.Char(
+        string='Deskripsi Pelaminan',
+        required=False)
+    harga = fields.Integer(
+        string='Harga Sewa',
+        required=False)
