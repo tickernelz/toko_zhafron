3c66d38ce38651ea7f79f90dfc7cbb6c0b82281d
tickernelz <zhafronadani@gmail.com>
2022-03-15 22:59:04 +0700

    update model panggung
    Signed-off-by: tickernelz <zhafronadani@gmail.com>

--- models/panggung.py
+++ models/panggung.py
@@ -6,17 +6,29 @@ class Panggung(models.Model):
     _description = 'Panggung'
 
     name = fields.Char(
-        string='Name')
-    pelaminan = fields.Char(
+        string='Nama Panggung',
+        required=True,)
+    pelaminan = fields.Many2one(
+        comodel_name='toko.pelaminan',
         string='Tipe Pelaminan',
+        domain="[('harga', '>', '0')]",
         required=False)
     accesories = fields.Char(
         string='Accesories Pelaminan',
-        required=False)
-    harga = fields.Integer(
-        string='Harga Sewa',
         required=False)
     bunga = fields.Selection(
         string='Tipe Bunga',
         required=False,
         selection=[('bunga mati', 'Bunga Mati'), ('bunga hidup', 'Bunga Hidup')])
+    harga = fields.Char(
+        compute='_compute_harga',
+        string='Harga Sewa',
+        required=False)
+
+    @api.depends('pelaminan')
+    def _compute_harga(self):
+        for record in self:
+            record.harga = record.pelaminan.harga
+
+
+
