43a3d9b0ae754a1d79dd29a92de313d4dc703f0a
tickernelz <zhafronadani@gmail.com>
2022-03-15 20:20:37 +0700

    cleanup useless files
    Signed-off-by: tickernelz <zhafronadani@gmail.com>

--- __manifest__.py
+++ __manifest__.py
@@ -24,12 +24,10 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+
     ],
 }
